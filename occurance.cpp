3e7f0f741db01323bd66e7ec6dceb156c488c904
dmanasa6 <manasadevarapalli6@gmail.com>
2018-12-06 15:39:50 +0530

    Add README.md

---
+++ occurance.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		int n;
+		cin >> n;
+		int count=0;
+		while(n>0)
+		{
+			if((n%10)==4)
+				count=count+1;
+			n=n/10;
+		}
+		cout << count << endl;
+	}
+	return 0;
+}
